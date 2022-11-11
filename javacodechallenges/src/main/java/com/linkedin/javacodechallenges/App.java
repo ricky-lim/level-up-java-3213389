8ac1ecfbf5ec6b4e58800141ff029ca140112abb
Ricky Lim <rlim.email@gmail.com>
2022-11-11 18:46:20 +0000

    Add isPasswordComplex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        return password.length() >= 6 && 
+            password.chars().anyMatch(Character::isUpperCase) &&
+            password.chars().anyMatch(Character::isLowerCase) && 
+            password.chars().anyMatch(Character::isDigit);
     }
 
     public static void main(String[] args) {
