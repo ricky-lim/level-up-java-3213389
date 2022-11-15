361f01725a86734c1983d560f861310e12ab4705
Ricky Lim <rlim.email@gmail.com>
2022-11-15 19:23:03 +0000

    Implement DoubleOrNothing

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,71 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
+  private int startPoint = 10;
+  private Scanner in = new Scanner(System.in);
+  private Random dice = new Random();
+  private int current = startPoint;
+  private boolean isStillPlaying = true;
 
   public void playGame() {
-    // TODO: Implement method
+    printWelcome();
+    askUserToContinue();
+
+    while (this.isStillPlaying) {
+      printTryToDouble();
+
+      if (isGettingDouble()) {
+        doublePoint();
+        congrats();
+        askUserToContinue();
+      } else {
+        printLost();
+        return;
+      }
+    }
+
+    printGoodbye();
+
   }
+
+  private void printLost() {
+    System.out.println("Oh no. You've lost. No points for you!");
+  }
+
+  private void congrats() {
+    System.out.println("Congrats now you have " + this.current + " points.");
+  }
+
+  private void printTryToDouble() {
+    System.out.println("Let's try to double it!");
+  }
+
+  protected void askUserToContinue() {
+    System.out.println("Would you like to continue with your luck? [Y/N] ");
+    this.isStillPlaying = this.in.nextLine().toUpperCase().equals("Y");
+  }
+
+  private void doublePoint() {
+    this.current = this.current * 2;
+  }
+
+  private boolean isGettingDouble() {
+    return this.dice.nextInt(2) == 0;
+  }
+
+  private void printWelcome() {
+    System.out.println("Let's play Double or Nothing. Your starting point: " + this.startPoint);
+  }
+
+  private void printGoodbye() {
+    System.out.println("Congrats you finish with " + this.startPoint + " points");
+  }
+
+  public boolean getIsPlaying() {
+    return isStillPlaying;
+  }
+
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,57 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    private final ByteArrayOutputStream gamePrintOut = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(gamePrintOut));
+    }
+
+    @After
+    public void restoreStream() {
+        System.setOut(System.out);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void askUserToContinue_Y() {
+        String input = "Y";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        DoubleOrNothing game = new DoubleOrNothing();
+        assertTrue(game.getIsPlaying());
+
+        game.askUserToContinue();
+        assertTrue(game.getIsPlaying());
+    }
+
+    @Test
+    public void askUserToContinue_N() {
+        String input = "N";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        DoubleOrNothing game = new DoubleOrNothing();
+        assertTrue(game.getIsPlaying());
+
+        game.askUserToContinue();
+        assertFalse(game.getIsPlaying());
     }
 }
